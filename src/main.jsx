0716ed56ddd35bef311dba285210e475ef0e513a
isworldnew <isworldnew@mail.ru>
2025-03-26 23:36:20 +0400

    init commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <Router basename="">
+          <Routes>
+              {/* <Route path="" element={< />} /> */}
+          </Routes>
+      </Router>
+  </React.StrictMode>
+);
