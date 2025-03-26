2f007857da0a3ffd6f6ba40e6a0b536ea30083cc
isworldnew <isworldnew@mail.ru>
2025-03-27 00:16:42 +0400

    try to run pipeline

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Router basename="">
           <Routes>
+            
               {/* <Route path="" element={< />} /> */}
           </Routes>
       </Router>
