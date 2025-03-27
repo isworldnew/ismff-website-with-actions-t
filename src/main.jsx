d43f1d0f4ec95b3ca52f4e192da80caae6b1f372
isworldnew <isworldnew@mail.ru>
2025-03-27 16:22:18 +0400

    Code & GitHub Pages Deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import HelloPage from './Pages/HelloPage/HelloPage'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Router basename="">
+      <Router basename="/ismff-website-with-actions-t">
           <Routes>
-              {/* <Route path="" element={< />} /> */}
+              <Route path="/" element={<HelloPage />} />
           </Routes>
       </Router>
   </React.StrictMode>
